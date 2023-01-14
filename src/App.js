def4662c9c2bd81547edbf552907d1340f621b59
Guido <guidopasciucco@gmail.com>
2023-01-14 11:34:31 -0300

    #1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home.js'
+import Footer from './components/footer.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Home/>
+      <Footer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return <>
+    <h1><i>1994</i></h1>
+    </>
+}
+
+export default Home
