d7fd13552e117947d407d7609a2b5927f5da59f2
Guido <guidopasciucco@gmail.com>
2023-01-23 12:23:04 -0300

    changes on music section

--- src/components/pages/music.js
+++ src/components/pages/music.js
@@ -5,56 +5,69 @@ import aeioucover from '../../assets/aeioucover.jpg';
 import tapanueva from '../../assets/tapanueva.jpg';
 
 const Music = () => {
-    return <><h3>ESTO ES EL MUSIC</h3>
-    <div>
-        <img src={ep1cover} alt="cover EP 1" width={400} />
-        <h3>EP 1</h3>
-        <h5>Diciembre 2015</h5>
-        <p>primeros temas creados durante 2015</p>
-        <ul>
-            <li>mensaje</li>
-            <li>a las 4</li>
-            <li>nada que hacer</li>
-            <li>en mi mente</li>
-            <li>historias de mentirita</li>
-        </ul>
-    </div>
-    <div>
-        <img src={aeioucover} alt="aeiou" width={400} />  
-        <h3>AEIOU (simple)</h3>
-        <h5>Enero 2017</h5>
-        <p>Parte del compilado Música para Merendar #1 de The merienda club</p>
-        <ul>
-            <li>AEIOU</li>
-        </ul>
-    </div>
-    <div>
-        <img src={ep2cover} alt="cover EP 2" width={400}/>
-        <h3>EP 2</h3>
-        <h5>Marzo 2017</h5>
-        <p>temas que surgieron durante 2016 mientras tocábamos</p>
-        <ul>
-            <li>al abrir los ojos</li>
-            <li>el sol del invierno</li>
-            <li>sabes</li>
-            <li>chino</li>
-        </ul>
-    </div>
-    <div>
-        <img src={tapanueva} alt="cover EP 2" width={400}/>
-        <h3>LP 1</h3>
-        <h5>Junio 2023</h5>
-        <p>primer disco largo, símbolo de una nueva etapa luego de 5 años de inactividad</p>
-        <ul>
-            <li>esta vez es para siempre</li>
-            <li>lejos de la ciudad</li>
-            <li>llgjfd lksdfjfglkfd</li>
-            <li>ad dsfklgjsfd gagad</li>
-            <li>udad</li>
-            <li>llgjfdlkfd</li>
-            <li>ad dsfklgjsfd g</li>
-        </ul>
-    </div>
+    return <>
+        <h3>ESTO ES EL MUSIC</h3>
+        <div className='container'>
+            <div className='row'>        
+                <div className='col-6'>
+                    <img src={ep1cover} alt="cover EP 1" width={400} />
+                </div>
+                <div className='col-6'>
+                    <h3>EP 1</h3>
+                    <h5>Diciembre 2015</h5>
+                    <p>primeros temas creados durante 2015</p>
+                    <ul>
+                        <li>mensaje</li>
+                        <li>a las 4</li>
+                        <li>nada que hacer</li>
+                        <li>en mi mente</li>
+                        <li>historias de mentirita</li>
+                    </ul>
+                </div>
+                <div className='col-6'>
+                    <img src={aeioucover} alt="aeiou" width={400} />  
+                </div>
+                <div className='col-6'>
+                    <h3>AEIOU (simple)</h3>
+                    <h5>Enero 2017</h5>
+                    <p>Parte del compilado Música para Merendar #1 de The merienda club</p>
+                    <ul>
+                        <li>AEIOU</li>
+                    </ul>
+                </div>
+                <div className='col-6'>
+                    <img src={ep2cover} alt="cover EP 2" width={400}/>
+                </div>
+                <div className='col-6'>
+                    <h3>EP 2</h3>
+                    <h5>Marzo 2017</h5>
+                    <p>temas que surgieron durante 2016 mientras tocábamos</p>
+                    <ul>
+                        <li>al abrir los ojos</li>
+                        <li>el sol del invierno</li>
+                        <li>sabes</li>
+                        <li>chino</li>
+                    </ul>
+                </div>
+                <div className='col-6'>
+                    <img src={tapanueva} alt="cover EP 2" width={400}/>
+                </div>
+                <div className='col-6'>
+                    <h3>LP 1</h3>
+                    <h5>Junio 2023</h5>
+                    <p>primer disco largo, símbolo de una nueva etapa luego de 5 años de inactividad</p>
+                    <ul>
+                        <li>esta vez es para siempre</li>
+                        <li>lejos de la ciudad</li>
+                        <li>llgjfd lksdfjfglkfd</li>
+                        <li>ad dsfklgjsfd gagad</li>
+                        <li>udad</li>
+                        <li>llgjfdlkfd</li>
+                        <li>ad dsfklgjsfd g</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </>
 }
 
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAZh0pcpaK2dzwS7a4KTpT2ybVIWJXKMC4",
+    authDomain: "site-607f9.firebaseapp.com",
+    projectId: "site-607f9",
+    storageBucket: "site-607f9.appspot.com",
+    messagingSenderId: "935838485995",
+    appId: "1:935838485995:web:0abbbf1705c17264d31dae"
+};
